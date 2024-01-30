31990ced38f723c4d6cb605103f0328eb23f7ff4
NicholasLimHongDa <nicholas.lim.hong.da.cs@gmail.com>
2024-01-30 16:42:57 +0900

    Initial commit, first version of my own primitive Bluetooth Utility Console App!

---
+++ Bluetooth/BluetoothConnector.cs
@@ -0,0 +1,146 @@
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brecham.Obex;
+
+namespace Bluetooth
+{
+
+    class BluetoothConnector
+    {
+        BluetoothDeviceInfo[] _devices;
+
+        public void scanRemoteDevices()
+        {
+            BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
+            BluetoothClient client = new BluetoothClient();
+            _devices = client.DiscoverDevicesInRange();
+        }
+
+        public void showDiscoveredDevices()
+        {
+            if (_devices == null)
+            {
+                Console.Write("Should be scanned first");
+            }
+            else
+            {
+                int i = 0;
+                foreach (BluetoothDeviceInfo device in _devices)
+                {
+                    Console.WriteLine("***********| " + i++ + " |***********");
+                    Console.WriteLine("Authenticated: " + device.Authenticated.ToString());
+                    Console.WriteLine("Class of Device: " + device.ClassOfDevice.ToString());
+                    Console.WriteLine("Connected: " + device.Connected.ToString());
+                    Console.WriteLine("Device Address: " + device.DeviceAddress.ToString());
+                    Console.WriteLine("Device Name: " + device.DeviceName.ToString());
+                    Console.WriteLine("InstalledServices: " + device.InstalledServices.ToString());
+                    Console.WriteLine("Last seen: " + device.LastSeen.ToString());
+                    Console.WriteLine("Last used: " + device.LastUsed.ToString());
+                    Console.WriteLine("Remembered: " + device.Remembered.ToString());
+                    Console.WriteLine("RSSI: " + device.Rssi.ToString());
+                }
+            }
+        }
+
+        public void sendFile(String pathToFile, int index)
+        {
+            bool fileExists = pathToFile != null;
+            bool correctDeviceRange = inRangeIndex(index);
+
+            if (fileExists && correctDeviceRange)
+            {
+                var file = @pathToFile;
+                var uri = new Uri("obex://" + _devices[index].DeviceAddress + "/" + file);
+                var request = new ObexWebRequest(uri);
+                request.ReadFile(file);
+                var response = (ObexWebResponse) request.GetResponse();
+                Console.WriteLine(response.StatusCode.ToString());
+                response.Close();
+            }
+            else
+            {
+                Console.WriteLine("Wrong parameters");
+            }
+        }
+
+        private bool inRangeIndex(int index)
+        {
+            bool correctRange = (index >=0 && index < _devices.Length);
+            if (!correctRange)
+            {
+                Console.WriteLine("Wrong scope");
+            }
+            return correctRange;
+        }
+
+        public void receiveFile()
+        {
+            var listener = new ObexListener(ObexTransport.Bluetooth);
+            listener.Start();
+            ObexListenerContext context = listener.GetContext();
+            ObexListenerRequest request = context.Request;
+            String[] pathSplits = request.RawUrl.Split('/');
+            String filename = pathSplits[pathSplits.Length - 1];
+            request.WriteFile(filename);
+            listener.Stop();
+            //Console.WriteLine("A file has been received!");
+            Console.WriteLine($"A file has been received at {filename}!");
+        }
+
+
+        public void showAllRadios()
+        {
+            BluetoothRadio[] radios = BluetoothRadio.AllRadios;
+            foreach (BluetoothRadio btradio in radios)
+            {
+                showRadioInfo(btradio);
+            }
+
+        }
+
+        public void showRadioInfo(BluetoothRadio radio = null)
+        {
+            if (radio == null)
+            {
+                radio = BluetoothRadio.PrimaryRadio;
+            }
+
+            if (radio != null)
+            {
+                RadioMode mode = radio.Mode;
+                Console.WriteLine("***************************");
+                Console.WriteLine("Name: " + radio.Name);
+                Console.WriteLine("Manufacturer: " + radio.Manufacturer);
+                Console.WriteLine("Class of device: " + radio.ClassOfDevice);
+                Console.WriteLine("Hardware status: " + radio.HardwareStatus);
+                Console.WriteLine("Local address: " + radio.LocalAddress);
+                Console.WriteLine("Software Manufacturer: " + radio.SoftwareManufacturer);
+            }
+            else
+            {
+                Console.WriteLine("No primary radio");
+            }
+        }
+
+        public String getPrimaryRadioAddress()
+        {
+            BluetoothRadio primaryRadio = BluetoothRadio.PrimaryRadio;
+            if (primaryRadio != null)
+            {
+                RadioMode mode = primaryRadio.Mode;
+                return primaryRadio.LocalAddress.ToString();
+            }
+            else
+            {
+                throw new Exception("No primary radio");
+            }
+        }
+    }
+}
---
+++ Bluetooth/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluetooth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.startMenu();
+            Console.ReadLine();
+        }
+
+        private void startMenu()
+        {
+            Console.WriteLine("BluetoothConnector 0.1");
+
+            BluetoothConnector bc = new BluetoothConnector();
+            int choice;
+            bool condition = true;
+            do
+            {
+                Console.Write("----------------------------------------------------\n");
+                Console.WriteLine("What do you want to do? :\n");
+                Console.Write(" 1 - Find a bluetooth adapter\n");
+                Console.Write(" 2 - Find bluetooth devices\n");
+                Console.Write(" 3 - Send file \n");
+                Console.Write(" 4 - Receive file \n");
+                Console.Write(" 0 - Finish\n");
+                Console.Write("----------------------------------------------------\n");
+                Console.Write(" Your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nFinding adapters...");
+
+                        bc.showAllRadios();
+                        break;
+                    case 2:
+                        Console.WriteLine("\nSearching for devices...");
+                        bc.scanRemoteDevices();
+                        bc.showDiscoveredDevices();
+                        break;
+                    case 3:
+                        Console.WriteLine("Sending a file");
+                        Console.WriteLine("What to send?");
+                        FileManager fileManager = new FileManager();
+                        fileManager.listFiles();
+                        int fileIndex = Convert.ToInt32(Console.ReadLine());
+                        string filePath = fileManager.getFilePath(fileIndex);
+
+                        Console.WriteLine("To whom?");
+                        bc.scanRemoteDevices();
+                        bc.showDiscoveredDevices();
+                        int deviceIndex = Convert.ToInt32(Console.ReadLine());
+                        bc.sendFile(filePath, deviceIndex);
+                        break;
+                    case 4:
+                        Console.WriteLine("Receiving a file");
+                        bc.receiveFile();
+                        break;
+
+                    case 0:
+                        Console.Write("Ending the program");
+                        condition = false;
+                        break;
+                    default:
+                        Console.Write("Invalid selection\n");
+                        break;
+                }
+            } while (condition != false);
+        }
+
+
+    }
+}
